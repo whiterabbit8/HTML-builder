2283f5ed16577d2153d499710421794c0ee4dce9
Natallia Ivanyuk <natashaivanyuk@gmail.com>
2023-05-09 15:51:07 +0300

    fix: delete file from copy folder if it is not exist in source folder

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -6,9 +6,21 @@ fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, (err) => {
   if (err) console.log(err);
 });
 
-fs.readdir(path.join(__dirname, 'files'), (err, files) => {
+fs.readdir(path.join(__dirname, 'files-copy'), (err, copies) => {
   if (err) console.log(err);
-  files.forEach((file) => {
-    fsPromises.copyFile(path.join(__dirname, 'files', file), path.join(__dirname, 'files-copy', file));
+
+  fs.readdir(path.join(__dirname, 'files'), (err, files) => {
+    if (err) console.log(err);
+    files.forEach((file) => {
+      fsPromises.copyFile(path.join(__dirname, 'files', file), path.join(__dirname, 'files-copy', file));
+    });
+
+    copies.forEach(copy => {
+      if (!files.includes(copy)) {
+        fs.rm(path.join(__dirname, 'files-copy', copy), (err) => {
+          if (err) console.log(err);
+        });
+      }
+    });
   });
 });
